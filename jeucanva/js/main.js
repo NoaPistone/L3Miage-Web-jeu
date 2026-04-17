0de7570d8e3b1188c558246b4e3b9228c629b075
Pistone Noa <noa.pistone@gmail.com>
2026-04-17 16:56:44 +0200

    test canva web

--- jeucanva/js/main.js
+++ jeucanva/js/main.js
@@ -1,7 +1,6 @@
-import Jeux from "../game/Jeux.js";
-let canvas = document.querySelector("#Canvas");
+import Jeux from "./game/Jeux.js";
+
+let canvas = document.querySelector("#Canvas"); // Vérifie bien la majuscule 'C' dans ton index.html
 let game = new Jeux(canvas);
 game.init();
 game.start();
-
-
